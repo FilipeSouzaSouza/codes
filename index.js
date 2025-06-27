726ed1dc6733e025b71b0c6bdb86478759c96a47
FilipeSouzaSouza <filipe.souza@mundiale.com.br>
2025-06-27 13:09:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import Component from "./src/Component.svelte";
+import Wrapper from "./lib/Wrapper.svelte"
+import schema from "./schema.json"
+import pkg from "./package.json"
 
-export default Component;
+if (window) {
+    const plugin = { Component: Wrapper, schema, version: pkg.version }
+    if (!window["##BUDIBASE_CUSTOM_COMPONENTS##"]) {
+        window["##BUDIBASE_CUSTOM_COMPONENTS##"] = []
+    }
+    window["##BUDIBASE_CUSTOM_COMPONENTS##"].push(plugin)
+    if (window.registerCustomComponent) {
+        window.registerCustomComponent(plugin)
+    }
+}
+
+export const Component = Wrapper
+export const version = pkg.version
+export { schema }
